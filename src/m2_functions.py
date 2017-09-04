8cd368dbbc4e4c932f2c555d4d1d1b2040019fce
chenh5 <chenh5@rose-hulman.edu>
2017-09-04 11:17:51 -0400

    objectsfunctionsmethods m2

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,12 +3,12 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Hui Chen.
 """
 
 ########################################################################
 #
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE and...
+# DONE: 1. PUT YOUR NAME IN THE ABOVE LINE and...
 #
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
@@ -34,6 +34,8 @@ def main():
     window = rg.TurtleWindow()
 
     turtle1()
+    turtle4()
+    turtle5()
     turtle3()
     turtle2()
     turtle2()
@@ -103,9 +105,27 @@ def turtle3():
     maja.end_fill()
 
 
+def turtle4():
+    hui = rg.SimpleTurtle()
+    hui.pen = rg.Pen('chocolate1', 6)
+    hui.forward(100)
+    hui.left(90)
+    hui.backward(100)
+    hui.right(90)
+    hui.forward(100)
+    hui.left(90)
+
+
+def turtle5():
+    chen = rg.SimpleTurtle()
+    chen.pen = rg.Pen('red',20)
+    chen.backward(200)
+    cow =rg.SimpleTurtle()
+    cow.pen = rg.Pen('blue',50)
+    cow.forward(300)
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   READ the code above.  Be sure you understand:
 #     -- How many functions are defined above?
 #           (Answer: 4)
@@ -137,7 +157,7 @@ def turtle3():
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Define another function,
 #   immediately below the end of the definition of   turtle3   above.
 #   Name your new function   turtle4.
@@ -167,7 +187,7 @@ def turtle3():
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a line to   main   that CALLS your new function immediately
 #   AFTER  main  calls turtle1.  So:
 #     -- the SimpleTurtle from turtle1 should move,
